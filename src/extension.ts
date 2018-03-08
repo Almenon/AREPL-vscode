7f273a824b406b5774e426e6831b0de6f5f594e4
Almenon <Almenon@users.noreply.github.com>
2018-03-07 21:13:54 -0800

    #13 added new arepl session command

--- src/extension.ts
+++ src/extension.ts
@@ -7,15 +7,26 @@ import pythonPreviewContentProvider from './HTMLDocumentContentProvider';
 export function activate(context: vscode.ExtensionContext) {
     // Register the commands that are provided to the user
     let disposableSidePreview = vscode.commands.registerCommand('extension.evalPythonInRealTime', () => {
-        createPreviewDoc(context, vscode.ViewColumn.Two); //show on right
+        createPreviewDoc(context);
     });
+
+    let newAREPLSession = vscode.commands.registerCommand('extension.newAREPLSession', async () => {
+        const cssDocument = await vscode.workspace.openTextDocument({
+            content: '',
+            language: 'python'
+        });
+    
+        vscode.window.showTextDocument(cssDocument).then(createPreviewDoc.bind(this, context));
+    });
+
     // push to subscriptions list so that they are disposed automatically
     context.subscriptions.push(disposableSidePreview);
+    context.subscriptions.push(newAREPLSession);
 }
 
-export function createPreviewDoc(context: vscode.ExtensionContext, viewColumn: vscode.ViewColumn) {
+export function createPreviewDoc(context: vscode.ExtensionContext) {
     let previewManager = new PreviewManager(context);
-    return vscode.commands.executeCommand('vscode.previewHtml', pythonPreviewContentProvider.PREVIEW_URI, viewColumn);
+    return vscode.commands.executeCommand('vscode.previewHtml', pythonPreviewContentProvider.PREVIEW_URI, vscode.ViewColumn.Two); //show on right
     // https://code.visualstudio.com/docs/extensionAPI/vscode-api-commands
 }
 
