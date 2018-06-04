6f047a81a3b51403ade064ab30fbd8e9721ba5ff
Almenon <almenon214@gmail.com>
2018-06-03 19:31:50 -0700

    #68 gui library as setting

--- src/pyGuiLibraryIsPresent.ts
+++ src/pyGuiLibraryIsPresent.ts
@@ -1,10 +1,17 @@
-let pyGuiLibraries = ["turtle", "matplotlib", "tkinter", "kivy", "pyforms", "PyQt4", "PyQt5", "wx", "pyside", "plotly", "ggplot", "bokeh"]
-// of course there are tons more GUI and plotting libraries but that should cover most
-// edge case: pandas may or may not be used for graphing
-let pyGuiLibrariesImport = new RegExp("^import (" + pyGuiLibraries.join('|') + ')', 'im')
-let pyGuiLibrariesFromImport = new RegExp("^from (" + pyGuiLibraries.join('|') + ')', 'im')
+import * as vscode from 'vscode'
 
 export default function pythonGuiLibraryIsPresent(code:string){
+
+	let settings = vscode.workspace.getConfiguration('AREPL')
+	let pyGuiLibraries = settings.get<string[]>('pyGuiLibraries')
+	pyGuiLibraries = pyGuiLibraries.filter(library => library.trim() != '')
+	if(pyGuiLibraries.length == 0){
+		return false
+	}
+
+	let pyGuiLibrariesImport = new RegExp("^import (" + pyGuiLibraries.join('|') + ')', 'im')
+	let pyGuiLibrariesFromImport = new RegExp("^from (" + pyGuiLibraries.join('|') + ')', 'im')
+
 	if(code.match(pyGuiLibrariesImport) || code.match(pyGuiLibrariesFromImport)){
 		return true
 	}
