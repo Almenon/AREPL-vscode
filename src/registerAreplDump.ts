c4ead59ce72851be4e9a77b2427ab7402e0b7d82
Almenon <Almenon@users.noreply.github.com>
2018-07-01 16:52:32 -0700

    fixing another unit test error

--- src/registerAreplDump.ts
+++ src/registerAreplDump.ts
@@ -1,6 +1,7 @@
 import { writeFileSync, existsSync, mkdirSync, unlinkSync } from "fs";
-import { join } from "path";
+import { join, sep, resolve, isAbsolute} from "path";
 import { execSync } from "child_process";
+
 
 /**
  * registers arepl dump python file w/ python so it can be imported
@@ -19,7 +20,7 @@ export function registerAreplDump(pythonPath:string=null, extensionDir:string){
 
     // i don't know why the heck site-packages folder would not exist
     // but it never hurts to be a little paranoid
-    if(!existsSync(sitePackagePath)) mkdirSync(sitePackagePath)
+    if(!existsSync(sitePackagePath)) mkDirByPathSync(sitePackagePath)
 
     writeFileSync(join(sitePackagePath, "areplDump.pth"), join(extensionDir, 'node_modules', 'arepl-backend', 'python'))
 }
@@ -37,4 +38,28 @@ function getsitePackagePath(pythonPath:string){
     // for some godforsaken reason it returns the path with a space at the end
     // hence the trim
     return execSync(pythonPath + " -m site --user-site").toString().trim()
+}
+
+/**
+ * node's inbuilt mkdirSync can't create multiple folders (wtf!). So i got this func from stackoverflow: https://stackoverflow.com/questions/31645738/how-to-create-full-path-with-nodes-fs-mkdirsync
+ */
+function mkDirByPathSync(targetDir:string, { isRelativeToScript = false } = {}) {
+    const initDir = isAbsolute(targetDir) ? sep : '';
+    const baseDir = isRelativeToScript ? __dirname : '.';
+
+    targetDir.split(sep).reduce((parentDir, childDir) => {
+        const curDir = resolve(baseDir, parentDir, childDir);
+        try {
+            mkdirSync(curDir);
+            console.log(`Directory ${curDir} created!`);
+        } catch (err) {
+            if (err.code !== 'EEXIST') {
+                throw err;
+            }
+
+            console.log(`Directory ${curDir} already exists!`);
+        }
+
+        return curDir;
+    }, initDir);
 }
