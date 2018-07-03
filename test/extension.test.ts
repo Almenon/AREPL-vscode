5e55e4b233dbe4ff28378316ffc3db0543ef16f5
Almenon <Almenon@users.noreply.github.com>
2018-07-02 20:39:43 -0700

    #79 linting tests

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -2,17 +2,17 @@
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
 //
-import * as assert from 'assert';
-import * as vscode from 'vscode';
+import * as assert from "assert";
+import * as vscode from "vscode";
 
-import {PreviewContainer} from '../src/previewContainer' 
-import Reporter from '../src/telemetry';
+import {PreviewContainer} from "../src/previewContainer" 
+import Reporter from "../src/telemetry";
 
 suite("Extension Tests", () => {
     
-    let mockContext: any = {
-        'asAbsolutePath': (file:string)=>{
-            return __dirname + '/' + file
+    const mockContext: any = {
+        asAbsolutePath: (file: string)=>{
+            return __dirname + "/" + file
         }
     }
 
@@ -20,21 +20,21 @@ suite("Extension Tests", () => {
     // todo: fix that crap
 
     test("command activates", (done) => {
-        vscode.commands.executeCommand('extension.newAREPLSession').then(()=>{
+        vscode.commands.executeCommand("extension.newAREPLSession").then(() => {
             // what we SHOULD be doing here is getting a promise from the command 
             // and asserting once the promise is resolved
             // but despite returning a promise it comes in as undefined... ugggg
             // assert.equal(vscode.window.activeTextEditor, true, "command failed to create new file")
             done()
-        }, (reason)=>{
+        }, reason => {
             assert.fail("failure executing command! reason: " + reason);
         })
     });
 
     test("print", (done) => {
-        let doc = new PreviewContainer(new Reporter(false), mockContext);
-        doc.onDidChange((x)=>{
-            let html = doc.provideTextDocumentContent(null);
+        const doc = new PreviewContainer(new Reporter(false), mockContext);
+        doc.onDidChange((x) =>{
+            const html = doc.provideTextDocumentContent(null);
             assert.equal(html.includes("hello world"), true, html);
             done()
         })
@@ -42,39 +42,63 @@ suite("Extension Tests", () => {
     });
 
     test("spawn error", (done) => {
-        let doc = new PreviewContainer(new Reporter(false), mockContext);
+        const doc = new PreviewContainer(new Reporter(false), mockContext);
         doc.onDidChange((x)=>{
-            let html = doc.provideTextDocumentContent(null);
+            const html = doc.provideTextDocumentContent(null);
             assert.equal(html.includes("Error in the AREPL extension"), true, html);
             done()
         })
-        doc.handleSpawnError("python3","C:\\dev\\python","error")
+        doc.handleSpawnError("python3", "C:\\dev\\python","error")
     });
 
     test("error", (done) => {
-        let doc = new PreviewContainer(new Reporter(false), mockContext);
-        doc.onDidChange((x)=>{
-            let html = doc.provideTextDocumentContent(null);
+        const doc = new PreviewContainer(new Reporter(false), mockContext);
+        doc.onDidChange((x) => {
+            const html = doc.provideTextDocumentContent(null);
             assert.equal(html.includes("yo"), true, html);
             done()
         })
-        doc.handleResult({userError:"yo",userVariables:{},execTime:0,totalPyTime:0,totalTime:0, internalError: "", caller: "", lineno: -1, done: true})
+        doc.handleResult(
+            {
+                caller: "",
+                done: true,
+                execTime: 0,
+                internalError: "",
+                lineno: -1,
+                totalPyTime: 0,
+                totalTime: 0,
+                userError: "yo",
+                userVariables: {},
+            }
+        )
     });
 
     test("userVariables", (done) => {
-        let doc = new PreviewContainer(new Reporter(false), mockContext);
-        doc.onDidChange((x)=>{
-            let html = doc.provideTextDocumentContent(null);
+        const doc = new PreviewContainer(new Reporter(false), mockContext);
+        doc.onDidChange((x) => {
+            const html = doc.provideTextDocumentContent(null);
             assert.equal(html.includes("5"), true, html);
             done()
         })
-        doc.handleResult({userError:"",userVariables:{'x':5},execTime:0,totalPyTime:0,totalTime:0, internalError: "", caller: "", lineno: -1, done: true})
+        doc.handleResult(
+            {
+                caller: "",
+                done: true,
+                execTime: 0,
+                internalError: "",
+                lineno: -1,
+                totalPyTime: 0,
+                totalTime: 0,
+                userError: "",
+                userVariables: {x: 5},
+        }
+        )
     });
 
     test("print escapes html", (done) => {
-        let doc = new PreviewContainer(new Reporter(false), mockContext);
-        doc.onDidChange((x)=>{
-            let html = doc.provideTextDocumentContent(null);
+        const doc = new PreviewContainer(new Reporter(false), mockContext);
+        doc.onDidChange((x) => {
+            const html = doc.provideTextDocumentContent(null);
             assert.equal(html.includes("&lt;module&gt;"), true, html);
             done()
         })
