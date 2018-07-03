20d258e989a9ba004a75c373079b6d7072c4a44f
Almenon <Almenon@users.noreply.github.com>
2018-07-02 20:01:28 -0700

    #79 linting pyGuiLibraryIsPresent.ts

--- src/pyGuiLibraryIsPresent.ts
+++ src/pyGuiLibraryIsPresent.ts
@@ -1,19 +1,19 @@
-import * as vscode from 'vscode'
+import * as vscode from "vscode"
 
-export default function pythonGuiLibraryIsPresent(code:string){
+export default function pythonGuiLibraryIsPresent(code: string){
 
-	let settings = vscode.workspace.getConfiguration('AREPL')
-	let pyGuiLibraries = settings.get<string[]>('pyGuiLibraries')
-	pyGuiLibraries = pyGuiLibraries.filter(library => library.trim() != '')
-	if(pyGuiLibraries.length == 0){
-		return false
-	}
+    const settings = vscode.workspace.getConfiguration("AREPL")
+    let pyGuiLibraries = settings.get<string[]>("pyGuiLibraries")
+    pyGuiLibraries = pyGuiLibraries.filter(library => library.trim() != "")
+    if (pyGuiLibraries.length == 0){
+        return false
+    }
 
-	let pyGuiLibrariesImport = new RegExp("^import (" + pyGuiLibraries.join('|') + ')', 'im')
-	let pyGuiLibrariesFromImport = new RegExp("^from (" + pyGuiLibraries.join('|') + ')', 'im')
+    const pyGuiLibrariesImport = new RegExp("^import (" + pyGuiLibraries.join("|") + ")", "im")
+    const pyGuiLibrariesFromImport = new RegExp("^from (" + pyGuiLibraries.join("|") + ")", "im")
 
-	if(code.match(pyGuiLibrariesImport) || code.match(pyGuiLibrariesFromImport)){
-		return true
-	}
-	else return false
+    if(code.match(pyGuiLibrariesImport) || code.match(pyGuiLibrariesFromImport)){
+      return true
+    }
+    else return false
 }
