afe6e5d4352c5b8cf1ab874fc207da81544033ec
Almenon <Almenon@users.noreply.github.com>
2018-07-02 18:44:04 -0700

    #79 linting extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,27 @@
 'use strict';
+import * as vscode from "vscode";
 import PreviewManager from "./PreviewManager"
 import pythonPreviewContentProvider from "./pythonPreview";
 import { registerAreplDump, unregisterAreplDump } from "./registerAreplDump";
 import Utilities from "./utilities";
-import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
 
-    let settings = vscode.workspace.getConfiguration('AREPL');
-    registerAreplDump(settings.get<string>('pythonPath'), context.extensionPath)
+    const settings = vscode.workspace.getConfiguration("AREPL");
+    registerAreplDump(settings.get<string>("pythonPath"), context.extensionPath)
 
     // Register the commands that are provided to the user
-    let arepl = vscode.commands.registerCommand('extension.evalPythonInRealTime', () => {
+    const arepl = vscode.commands.registerCommand("extension.evalPythonInRealTime", () => {
         createPreviewDoc(context);
     });
 
-    let newAreplSession = vscode.commands.registerCommand('extension.newAREPLSession', ()=>{
+    const newAreplSession = vscode.commands.registerCommand("extension.newAREPLSession", ()=>{
         Utilities.newUnsavedPythonDoc(Utilities.getHighlightedText())
             .then(createPreviewDoc.bind(this, context));
     });
 
     // exact same as above, just defining command so users are aware of the feature
-    let areplOnHighlightedCode = vscode.commands.registerCommand('extension.newAREPLSessionOnHighlightedCode', ()=>{
+    const areplOnHighlightedCode = vscode.commands.registerCommand("extension.newAREPLSessionOnHighlightedCode", ()=>{
         Utilities.newUnsavedPythonDoc(Utilities.getHighlightedText())
             .then(createPreviewDoc.bind(this, context));
     });
@@ -33,16 +33,19 @@ export function activate(context: vscode.ExtensionContext) {
 
 export function createPreviewDoc(context: vscode.ExtensionContext) {
 
-    let previewManager = new PreviewManager(context);
+    const previewManager = new PreviewManager(context);
     previewManager.startArepl()
     
-    return vscode.commands.executeCommand('vscode.previewHtml', pythonPreviewContentProvider.PREVIEW_URI, vscode.ViewColumn.Two); //show on right
+    // viewcolum 2 to show on right
+    return vscode.commands.executeCommand(
+        "vscode.previewHtml", 
+        pythonPreviewContentProvider.PREVIEW_URI, vscode.ViewColumn.Two);
     // https://code.visualstudio.com/docs/extensionAPI/vscode-api-commands
 }
 
 
 // This method is called when extension is deactivated
 export function deactivate() {
-    let settings = vscode.workspace.getConfiguration('AREPL');
-    unregisterAreplDump(settings.get('pythonPath'))
+    const settings = vscode.workspace.getConfiguration("AREPL");
+    unregisterAreplDump(settings.get("pythonPath"))
 }
