fa018c2517a9e61d93a5cfc41462856c6399b284
Almenon <Almenon@users.noreply.github.com>
2018-11-15 19:46:12 -0800

    adding tests for utilities

---
+++ test/utilities.test.ts
@@ -0,0 +1,43 @@
+//
+// Note: This example test is leveraging the Mocha test framework.
+// Please refer to their documentation on https://mochajs.org/ for help.
+//
+import * as assert from "assert";
+import Utilities from '../src/utilities'
+
+suite("Utility Tests", () => {
+
+    test("isEmpty returns true for empty element", function(){
+        assert.equal(Utilities.isEmpty({}), true)
+        assert.equal(Utilities.isEmpty([]), true)
+    });
+
+    test("isEmpty returns false for non-empty elements", function(){
+        assert.equal(Utilities.isEmpty({a:null}), false)
+        assert.equal(Utilities.isEmpty([1]), false)
+    });
+
+    test("escape html escapes html", function(){
+        assert.equal(Utilities.escapeHtml("<yo>"), "&lt;yo&gt;")
+    });
+
+    test("escape html does not escape non-html", function(){
+        assert.equal(Utilities.escapeHtml("feaf$"), "feaf$")
+    });
+
+    test("new python doc", function(){
+        Utilities.newUnsavedPythonDoc("test").then((editor)=>{
+            assert.equal(editor.document.isClosed, false)
+            assert.equal(editor.document.getText(), "test")
+            // not sure how to close the document
+        })
+    });
+
+    // test("get highlighted text", function(){
+    //     Utilities.newUnsavedPythonDoc("testGetHighlightedText").then((editor)=>{
+    //         // not sure how to highlight text :/
+    //         assert.equal(Utilities.getHighlightedText(), "testGetHighlightedText")
+    //     })
+    // })
+
+});
