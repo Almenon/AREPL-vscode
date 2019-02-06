eb05e52a147d441c8015a56db5657495ca53e876
Caleb15 <caleb@15five.com>
2019-02-05 22:22:49 -0800

    add pandas test

---
+++ test/manualAreplTests/pandasTest.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+x = pd.DataFrame()
+print(x)
+
+"""
+-{
+    x: -{
+        py/object: "pandas.core.frame.DataFrame",
+        values: "\"\"\r\n",
+        txt: true,
+        meta: -{
+            dtypes: {},
+            index_col: 0
+        }
+    }
+}
+"""
