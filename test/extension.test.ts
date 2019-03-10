41e0c223bdf02ad2e148445c6139ce69516f0606
almenon <almenon214@gmail.com>
2019-03-10 12:06:31 -0700

    try out extensiont test again

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -3,18 +3,17 @@ import * as vscode from "vscode";
 
 suite("extension tests", function(){
     // this test causes others to fail. Commenting this out untill I fix it
-    // test("command activates", (done) => {
-    //     vscode.commands.executeCommand("extension.newAREPLSession").then((p) => {
-    //         // what we SHOULD be doing here is getting a promise from the command
-    //         // and asserting once the promise is resolved
-    //         // but despite returning a promise it comes in as undefined... ugggg
-    //         // assert.equal(vscode.window.activeTextEditor, true, "command failed to create new file")
-    //         assert.ok(true);
-    //
-    //         // close arepl preview here
-    //         done();
-    //     }, reason => {
-    //         done(reason);
-    //     })
-    // });
+     test("command activates", (done) => {
+         vscode.commands.executeCommand("extension.newAREPLSession").then((p) => {
+             // what we SHOULD be doing here is getting a promise from the command
+             // and asserting once the promise is resolved
+             // but despite returning a promise it comes in as undefined... ugggg
+             // assert.equal(vscode.window.activeTextEditor, true, "command failed to create new file")
+    
+             // close arepl preview here
+             done();
+         }, reason => {
+             done(reason);
+         })
+     });
 })
