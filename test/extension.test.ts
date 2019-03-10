03fd1858ce07a37b9025af36c9b97af0b4c37107
almenon <almenon214@gmail.com>
2019-03-10 12:17:34 -0700

    add comment

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -3,11 +3,14 @@ import * as vscode from "vscode";
 
 suite("extension tests", function(){
     // this test causes others to fail. Commenting this out untill I fix it
-     test("command activates", (done) => {
+    test("command activates", (done) => {
          vscode.commands.executeCommand("extension.newAREPLSession").then((p) => {
              // what we SHOULD be doing here is getting a promise from the command
              // and asserting once the promise is resolved
              // but despite returning a promise it comes in as undefined... ugggg
+
+             // for some reason uncommenting below assert causes timeout?
+             // even when setting timeout high like 10 seconds
              // assert.equal(vscode.window.activeTextEditor, true, "command failed to create new file")
     
              // close arepl preview here
