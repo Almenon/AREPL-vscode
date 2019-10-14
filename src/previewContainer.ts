3db2778670c496ee6e5aabf416820e341e6f1023
almenon <almenon214@gmail.com>
2019-10-13 19:55:12 -0700

    temporarily go back to old API

--- src/previewContainer.ts
+++ src/previewContainer.ts
@@ -102,7 +102,10 @@ export class PreviewContainer{
                 vscode.window.showErrorMessage("Internal AREPL Error: " + error.toString(), "Report bug").then((action)=>{
                     if(action == "Report bug"){
                         const bugReportLink = vscode.Uri.parse("https://github.com/Almenon/AREPL-vscode/issues/new")
-                        vscode.env.openExternal(bugReportLink)
+                        // enable below for vscode version 1.31.0 or higher
+                        // vscode.env.openExternal(bugReportLink)
+                        vscode.commands.executeCommand('vscode.open', bugReportLink)
+
                     }
                 })
             }
