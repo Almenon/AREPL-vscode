7ed81bd83b8b30b1d18366c7f1254b13b6dc6ba6
almenon <almenon214@gmail.com>
2019-10-23 20:30:50 -0700

    add files previously accidentily excluded

---
+++ src/env/application/workspace.ts
@@ -0,0 +1,43 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { CancellationToken, ConfigurationChangeEvent, Event, FileSystemWatcher, GlobPattern, Uri, workspace, WorkspaceConfiguration, WorkspaceFolder, WorkspaceFoldersChangeEvent } from 'vscode';
+import { Resource } from '../types';
+import { IWorkspaceService } from './types';
+
+export class WorkspaceService implements IWorkspaceService {
+    public get onDidChangeConfiguration(): Event<ConfigurationChangeEvent> {
+        return workspace.onDidChangeConfiguration;
+    }
+    public get rootPath(): string | undefined {
+        return Array.isArray(workspace.workspaceFolders) ? workspace.workspaceFolders[0].uri.fsPath : undefined;
+    }
+    public get workspaceFolders(): WorkspaceFolder[] | undefined {
+        return workspace.workspaceFolders;
+    }
+    public get onDidChangeWorkspaceFolders(): Event<WorkspaceFoldersChangeEvent> {
+        return workspace.onDidChangeWorkspaceFolders;
+    }
+    public get hasWorkspaceFolders() {
+        return Array.isArray(workspace.workspaceFolders) && workspace.workspaceFolders.length > 0;
+    }
+    public getConfiguration(section?: string, resource?: Uri): WorkspaceConfiguration {
+        return workspace.getConfiguration(section, resource || null);
+    }
+    public getWorkspaceFolder(uri: Resource): WorkspaceFolder | undefined {
+        return uri ? workspace.getWorkspaceFolder(uri) : undefined;
+    }
+    public asRelativePath(pathOrUri: string | Uri, includeWorkspaceFolder?: boolean): string {
+        return workspace.asRelativePath(pathOrUri, includeWorkspaceFolder);
+    }
+    public createFileSystemWatcher(globPattern: GlobPattern, _ignoreCreateEvents?: boolean, ignoreChangeEvents?: boolean, ignoreDeleteEvents?: boolean): FileSystemWatcher {
+        return workspace.createFileSystemWatcher(globPattern, ignoreChangeEvents, ignoreChangeEvents, ignoreDeleteEvents);
+    }
+    public findFiles(include: GlobPattern, exclude?: GlobPattern, maxResults?: number, token?: CancellationToken): Thenable<Uri[]> {
+        return workspace.findFiles(include, exclude, maxResults, token);
+    }
+    public getWorkspaceFolderIdentifier(resource: Resource, defaultValue: string = ''): string {
+        const workspaceFolder = resource ? workspace.getWorkspaceFolder(resource) : undefined;
+        return workspaceFolder ? workspaceFolder.uri.fsPath : defaultValue;
+    }
+}
---
+++ src/env/utils/version.ts
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+'use strict';
+
+import * as semver from 'semver';
+import { Version } from '../types';
+
+export function parseVersion(raw: string): semver.SemVer {
+    raw = raw.replace(/\.00*(?=[1-9]|0\.)/, '.');
+    const ver = semver.coerce(raw);
+    if (ver === null || !semver.valid(ver)) {
+        // tslint:disable-next-line: no-suspicious-comment
+        // TODO: Raise an exception instead?
+        return new semver.SemVer('0.0.0');
+    }
+    return ver;
+}
