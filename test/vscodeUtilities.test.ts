53be6e09177199fda16c848a8cfee15e1b0288a7
almenon <almenon214@gmail.com>
2020-02-09 10:48:49 -0800

    add more tests for expand path setting

--- test/vscodeUtilities.test.ts
+++ test/vscodeUtilities.test.ts
@@ -15,29 +15,36 @@ Module.prototype.require = function hijacked(file) {
 
 import * as assert from "assert";
 import vscodeUtilities from '../src/vscodeUtilities'
+import { sep } from 'path';
 
 describe('vscode utilities tests', ()=>{
 
     it('eol as string', ()=>{
         const d = new vscodeMock.TextDocument("", "")
-        assert.equal(vscodeUtilities.eol(<any>d), "\n")
+        assert.strictEqual(vscodeUtilities.eol(<any>d), "\n")
 
         d.eol = 2
-        assert.equal(vscodeUtilities.eol(<any>d), "\r\n")
+        assert.strictEqual(vscodeUtilities.eol(<any>d), "\r\n")
     })
 
     describe('expand path setting', () => {
         it('should replace env vars', () => {
             process.env["foo2435"] = "a"
-            assert.equal(vscodeUtilities.expandPathSetting("${env:foo2435}"), "a")
+            assert.strictEqual(vscodeUtilities.expandPathSetting("${env:foo2435}"), "a")
             delete process.env["foo2435"]
         });
 
-        it('expand path setting', () => {
-            const uri = new vscodeMock.Uri("","","a","","")
-            vscodeMock.workspace.workspaceFolders = [uri]
-            assert.equal(vscodeUtilities.expandPathSetting("${workspaceFolder}/foo"), "a/foo")
-            vscodeMock.workspace.workspaceFolders = []
+        it('should replace workspacefolder', () => {
+            const originalWorkspaceFolders = vscodeMock.workspace.workspaceFolders
+            assert.strictEqual(vscodeUtilities.expandPathSetting("${workspaceFolder}/foo"), "root/foo")
+        });
+
+        it('should make relative paths absolute', () => {
+            assert.strictEqual(vscodeUtilities.expandPathSetting(`foo${sep}.env`), `root${sep}foo${sep}.env`)
+        });
+        
+        it('should not change absolute paths', () => {
+            assert.strictEqual(vscodeUtilities.expandPathSetting(__dirname), __dirname)
         });
     });
 
