b1c1692cafa3046eedde69e53688290ad49d9f54
almenon <almenon214@gmail.com>
2020-02-22 11:32:15 -0800

    escape <> in changelog

--- src/pythonPanelPreview.ts
+++ src/pythonPanelPreview.ts
@@ -32,7 +32,7 @@ export default class PythonPanelPreview{
         <li>Help me make arepl better by filling out this short survey: <a href="https://forms.gle/m7xirfRnSRoPAe9e9">https://forms.gle/m7xirfRnSRoPAe9e9</a></li>
         <li>🚀 Sped up backend when pickling primitives</li>
         <li>🚀 defaultFilterVars setting added. You can use it to filter out vars of certain types from displaying in the variable view. You can also set a arepl_filter variable in arepl to play around with it in real-time. <code>arepl_filter=['foo']</code></li>
-        <li>🚀 defaultFilterTypes setting added. You can use it to filter out vars of certain types from displaying in the variable view. You can also set a arepl_filter_type variable in arepl to play around with it in real-time. <code>arepl_filter_type=["<class 'str'>"]</code></li>
+        <li>🚀 defaultFilterTypes setting added. You can use it to filter out vars of certain types from displaying in the variable view. You can also set a arepl_filter_type variable in arepl to play around with it in real-time. <code>arepl_filter_type=["\<class 'str'\>"]</code></li>
         <li>🚀 You can now define a arepl_filter_function variable you can use to totally customize the variables appearing in view</li>
         <li>🐛 [Fixed a TypeError with pandas thanks to David Aguilar](https://github.com/Almenon/AREPL-backend/issues/104)</li>
         <li>🔧 Added showNameErrors and showSyntaxErrors settings you can use to not show those errors if they annoy you</li>
