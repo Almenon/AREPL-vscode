42c56190bb34de04a6a3eceb07e7bbf4b2918aa5
almenon <almenon214@gmail.com>
2020-11-15 09:46:07 -0800

    revert error message
    I don't want to break things for users of 3.6 as it is still supported by python

--- src/PreviewManager.ts
+++ src/PreviewManager.ts
@@ -178,8 +178,7 @@ export default class PreviewManager {
     private warnIfOutdatedPythonVersion(pythonPath: string){
         PythonShell.getVersion(`"${pythonPath}"`).then((out)=>{
             let version = out.stdout ? out.stdout : out.stderr
-            if(version?.includes("Python 3.6") || version?.includes("Python 3.5")
-            || version?.includes("Python 3.4") || version?.includes("Python 2")){
+            if(version?.includes("Python 3.4") || version?.includes("Python 2")){
                 vscode.window.showErrorMessage(`AREPL no longer supports ${version}.
                 Please upgrade or set AREPL.pythonPath to a diffent python.
                 AREPL needs python 3.7 or greater`)
