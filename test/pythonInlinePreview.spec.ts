0a057c8647d4d5396017e7581a8f8191a34daddb
almenon <almenon214@gmail.com>
2020-11-15 11:29:45 -0800

    add inline test

---
+++ test/pythonInlinePreview.spec.ts
@@ -0,0 +1,55 @@
+//////////////////////////////////////////////
+// below thanks to https://github.com/rokucommunity/vscode-brightscript-language
+let Module = require('module');
+import { vscodeMock } from './mockVscode.spec';
+//override the "require" call to mock certain items
+const { require: oldRequire } = Module.prototype;
+Module.prototype.require = function hijacked(file) {
+    if (file === 'vscode') {
+        return vscodeMock;
+    } else {
+        return oldRequire.apply(this, arguments);
+    }
+};
+//////////////////////////////////////////////
+
+import * as assert from "assert";
+import PythonInlinePreview from '../src/pythonInlinePreview'
+import { UserError } from 'arepl-backend';
+
+suite('inline preview', ()=>{
+
+    test('shows one error if just one error', ()=>{
+        const context: any = {
+            'asAbsolutePath': (path)=>{}
+        }
+        let p = new PythonInlinePreview(null, context);
+
+        const error: UserError = {
+            __cause__: null,
+            __context__: null,
+            _str: "",
+            cause: null,
+            context: null,
+            exc_traceback: {},
+            exc_type: {
+                "py/type": "ValueError"
+            },
+            stack: {
+                "py/seq": [{
+                    _line: "raise ValueError()",
+                    filename: "<string>",
+                    lineno: 1,
+                    locals: {},
+                    name: "foo"
+                }]
+            }
+        }
+
+        vscodeMock.window.activeTextEditor.setDecorations = (decorationType, rangesOrOptions: any[])=>{
+            assert.strictEqual(rangesOrOptions.length, 1)
+        }
+        p.showInlineErrors(error, "")
+
+    })
+})
